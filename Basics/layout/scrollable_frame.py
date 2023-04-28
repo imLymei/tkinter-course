3a4632727e4d680caf5e655d235be6b0a7f0b41f
Lymei <lymeicontato@gmail.com>
2023-04-28 14:12:13 -0300

    Scrollable Frame update

---
+++ Basics/layout/scrollable_frame.py
@@ -0,0 +1,86 @@
+import tkinter as tk
+from tkinter import ttk
+
+
+class ListFrame(ttk.Frame):
+    def __init__(self, parent, text_data, item_height):
+        super().__init__(master=parent)
+        self.pack(expand=True, fill='both')
+
+        # widget data
+        self.text_data = text_data
+        self.item_number = len(text_data)
+        self.list_height = self.item_number * item_height
+
+        # canvas
+        self.canvas = tk.Canvas(self, background='red',
+                                scrollregion=(0, 0, self.winfo_width(), self.list_height))
+        self.canvas.pack(expand=True, fill='both')
+
+        # display frame
+        self.frame = ttk.Frame(self)
+
+        self.scrollbar = ttk.Scrollbar(self, command=self.canvas.yview)
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+        self.scrollbar.place(relx=1, rely=0, relheight=1, anchor='ne')
+
+        for index, item in enumerate(self.text_data):
+            self.create_item(index, item).pack(
+                expand=True, fill='both', pady=4, padx=10)
+
+        self.canvas.create_window(
+            (0, 0),
+            window=self.frame,
+            anchor='nw',
+            width=self.winfo_width(),
+            height=self.list_height
+        )
+
+        # events
+        self.canvas.bind_all(
+            '<MouseWheel>', lambda event: self.canvas.yview_scroll(-int(event.delta/60), 'units'))
+
+        self.bind('<Configure>', self.update_size)
+
+    def update_size(self, event):
+        if self.list_height >= self.winfo_height():
+            height = self.list_height
+            self.canvas.bind_all(
+                '<MouseWheel>', lambda event: self.canvas.yview_scroll(-int(event.delta/60), 'units'))
+        else:
+            height = self.winfo_height()
+            self.canvas.unbind_all('<MouseWheel>')
+
+        self.canvas.create_window((0, 0),
+                                  window=self.frame,
+                                  anchor='nw',
+                                  width=self.winfo_width(),
+                                  height=height
+                                  )
+
+    def create_item(self, index, item):
+        frame = ttk.Frame(self.frame)
+
+        # grid layout
+        frame.rowconfigure(0, weight=1)
+        frame.columnconfigure((0, 1, 2, 3, 4), weight=1, uniform='a')
+
+        # widgets
+        ttk.Label(frame, text=f'#{index}').grid(column=0, row=0)
+        ttk.Label(frame, text=f'{item[0]}').grid(column=1, row=0)
+        ttk.Button(frame, text=f'{item[1]}').grid(
+            column=2, row=0, columnspan=3, sticky='nswe')
+
+        return frame
+
+
+window = tk.Tk()
+window.geometry('500x400')
+window.title('Scrolling')
+
+text_list = [('label', 'button'), ('thing', 'click'),
+             ('third', 'something'), ('label 1', 'label 2'),
+             ('forth', 'i do not know'), ('just another label', 'just another button')]
+list_frame = ListFrame(window, text_list, 100)
+
+window.mainloop()
